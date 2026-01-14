f966098c8c38c50081ee0b1ccc8f013c279680e6
The-Infinitys <the.infinity.s.infinity@gmail.com>
2026-01-14 22:59:16 +0900

    fix: fix some

--- src/client/java/org/infinite/mixin/core/multiplayer/ClientLevelMixin.java
+++ src/client/java/org/infinite/mixin/core/multiplayer/ClientLevelMixin.java
@@ -2,6 +2,12 @@ package org.infinite.mixin.core.multiplayer;
 
 import java.util.function.BooleanSupplier;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.Holder;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.storage.WritableLevelData;
 import org.infinite.InfiniteClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,15 +15,27 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientLevel.class)
-public class ClientLevelMixin {
+@SuppressWarnings("Unused")
+public abstract class ClientLevelMixin extends Level {
+
+  protected ClientLevelMixin(
+      WritableLevelData writableLevelData,
+      ResourceKey<Level> resourceKey,
+      RegistryAccess registryAccess,
+      Holder<DimensionType> holder,
+      boolean bl,
+      boolean bl2,
+      long l,
+      int i) {
+    super(writableLevelData, resourceKey, registryAccess, holder, bl, bl2, l, i);
+  }
+
   @Inject(at = @At("HEAD"), method = "tick")
-  @SuppressWarnings("Unused")
   private void onStartTick(BooleanSupplier booleanSupplier, CallbackInfo ci) {
     InfiniteClient.INSTANCE.getWorldTicks().onStartTick();
   }
 
   @Inject(at = @At("TAIL"), method = "tick")
-  @SuppressWarnings("Unused")
   private void onEndTick(BooleanSupplier booleanSupplier, CallbackInfo ci) {
     InfiniteClient.INSTANCE.getWorldTicks().onEndTick();
   }
--- src/client/java/org/infinite/mixin/core/ui/SettingScreenMixin.java
+++
@@ -1,6 +0,0 @@
-package org.infinite.mixin.core.ui;
-
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin()
-public class SettingScreenMixin {}
