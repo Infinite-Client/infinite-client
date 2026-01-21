0790916f4cf3bfa4377fd13eec36396f38bc2e93
The-Infinitys <the.infinity.s.infinity@gmail.com>
2026-01-21 21:29:30 +0900

    feat: add two features

--- src/client/kotlin/org/infinite/infinite/features/local/movement/LocalMovementCategory.kt
+++ src/client/kotlin/org/infinite/infinite/features/local/movement/LocalMovementCategory.kt
@@ -1,5 +1,7 @@
 package org.infinite.infinite.features.local.movement
 
+import org.infinite.infinite.features.local.movement.bettersneak.BetterSneakFeature
+import org.infinite.infinite.features.local.movement.fastdescent.FastDescentFeature
 import org.infinite.infinite.features.local.movement.fly.SuperFlyFeature
 import org.infinite.infinite.features.local.movement.quickmove.QuickMoveFeature
 import org.infinite.infinite.features.local.movement.water.WaterHoveringFeature
@@ -7,6 +9,8 @@ import org.infinite.libs.core.features.categories.category.LocalCategory
 
 @Suppress("Unused")
 class LocalMovementCategory : LocalCategory() {
+    val betterSneakFeature by feature(BetterSneakFeature())
+    val fastDescentFeature by feature(FastDescentFeature())
     val superFlyFeature by feature(SuperFlyFeature())
     val quickMoveFeature by feature(QuickMoveFeature())
     val waterHoveringFeature by feature(WaterHoveringFeature())
